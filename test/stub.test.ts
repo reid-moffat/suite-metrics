609079b3f2b4c35269aedf3d92c8885e9e01c18c
Reid Moffat <18rem8@queensu.ca>
2024-08-06 16:06:01 -0400

    nodenext for imports

--- test/stub.test.ts
+++ test/stub.test.ts
@@ -1,7 +1,13 @@
 import { expect } from 'chai';
+import { GenerateArray } from 'generate-arrays';
 
 suite("Test suite", () => {
     test("Test test", () => {
         expect(true).to.equal(true);
+    });
+
+    test("Test test 2", () => {
+        const arr = GenerateArray.counting(1, 10, 1);
+        expect(arr).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
 });
