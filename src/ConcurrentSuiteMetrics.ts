71db4334440a822c6fc5ffa5771009744f0bbfc6
Reid Moffat <18rem8@queensu.ca>
2025-06-25 11:59:05 -0400

    packages + undo mutex

--- src/ConcurrentSuiteMetrics.ts
+++ src/ConcurrentSuiteMetrics.ts
@@ -59,31 +59,4 @@ class ConcurrentSuiteMetrics implements ISuiteMetrics {
 
 }
 
-class Mutex {
-    private _locked: boolean = false;
-    private _queue: Array<() => void> = [];
-
-    public async acquireAsync(): Promise<void> {
-        return new Promise<void>((resolve) => {
-            if (!this._locked) {
-                this._locked = true;
-                resolve();
-            } else {
-                this._queue.push(resolve);
-            }
-        });
-    }
-
-    public release(): void {
-        if (this._queue.length > 0) {
-            const nextResolve = this._queue.shift();
-            if (nextResolve) {
-                nextResolve();
-            }
-        } else {
-            this._locked = false;
-        }
-    }
-}
-
 export default ConcurrentSuiteMetrics;
