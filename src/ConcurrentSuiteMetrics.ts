1cdc5a3b00e3869340bb92f15b920bec76d7a97b
Reid Moffat <18rem8@queensu.ca>
2025-07-11 21:49:24 -0400

    comment

--- src/ConcurrentSuiteMetrics.ts
+++ src/ConcurrentSuiteMetrics.ts
@@ -79,6 +79,7 @@ class ConcurrentSuiteMetrics extends BaseSuiteMetrics {
             throw new Error(`Test [${path.join(', ')}] is not currently running. Call startTest() first to begin testing`);
         }
 
+        // Store test data and delete from active tests
         this.addTest(path, testStartTime, endTime);
         this.activeTests.delete(testKey);
     }
