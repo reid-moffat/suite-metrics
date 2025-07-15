00a4adc81596ef51f673d0791640e6b4d4a39429
Reid Moffat <18rem8@queensu.ca>
2025-07-15 14:30:41 -0400

    err handle

--- test/helpers.ts
+++ test/helpers.ts
@@ -7,6 +7,7 @@ function randomInt(min: number, max: number): number {
     if (min > max) {
         throw new Error(`[randomInt] Error: min (${min}) is greater than the value of max (${max})`);
     }
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -14,6 +15,10 @@ function randomInt(min: number, max: number): number {
  * Delay of the at least the specified number of milliseconds (uses microseconds to ensure precision)
  */
 function addDelay(millis: number): void {
+    if (millis <= 0) {
+        throw new Error(`[addDelay] Error: millis (${millis}) must be greater than 0`);
+    }
+
     const endTime: number = microtime.now() + millis * 1000;
     while (microtime.now() < endTime) { /* busy wait */ }
 }
