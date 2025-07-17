c8dd263dd6d036477774e8745af4cb0750b92534
Reid Moffat <18rem8@queensu.ca>
2025-07-17 15:42:56 -0400

    fixed stats ref for validate path

--- src/SuiteMetrics.ts
+++ src/SuiteMetrics.ts
@@ -49,7 +49,7 @@ class SuiteMetrics extends BaseSuiteMetrics {
                 'starting a new test, or use ConcurrentSuiteMetrics to run multiple tests simultaneously');
         }
 
-        this.validatePath(path, false);
+        BaseSuiteMetrics.validatePath(path, false);
 
         this.activeTest = {
             testPath: path,
