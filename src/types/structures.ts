f02c69a7dda8db1702a5da948533619fb1cb5d11
Reid Moffat <18rem8@queensu.ca>
2025-07-22 12:35:05 -0400

    structure type separate

---
+++ src/types/structures.ts
@@ -0,0 +1,43 @@
+/**
+ * A completed test's structure in suite metrics
+ *
+ * Include the test's name, start/end time & duration, overall test number, and test number within this suite
+ */
+type Test = {
+    /** The test's name. Determined during startTest(), this is unique for the suite it's in */
+    readonly name: string;
+    /** Timestamp the test began, in microseconds. Obtained at the end of startTest() for maximal accuracy */
+    readonly startTimestamp: number;
+    /** Timestamp the test ended at, in microseconds. Obtained at the beginning of stopTest() for maximal accuracy */
+    readonly endTimestamp: number;
+    /** Duration the test took, in microseconds. Equal to endTimestamp - startTimestamp */
+    readonly duration: number;
+    /** Order this test was completed in for all tests in any suite. E.g. 34th test of 150 -> 34 */
+    readonly testNumber: number;
+    /** Order this test was completed in for this suite. E.g. the 3rd of 6 test in the suite -> 3 */
+    readonly suiteTestNumber: number;
+};
+
+/**
+ * A test suite's structure in suite metrics
+ *
+ * Includes the suite's name, tests in the suite, sub-suites this suite has, and the number of tests in sub-suites
+ */
+type Suite = {
+    /** The suite's name */
+    readonly name: string;
+    /** All tests directly in this suite (doesn't include sub-suites). Maps the test name to the Test type object */
+    readonly tests: Map<string, Test>;
+    /** All sub-suites directly within this suite (non-recursive). Maps the suite name to the Suite type object */
+    readonly subSuites: Map<string, Suite>;
+
+    /** Stores aggregate test data from sub-suites to prevent the need for recursive calls */
+    readonly subSuiteData: {
+        /** Number of tests in this suite AND in all sub-suites of this suite */
+        numSubTests: number;
+        /** Total test time for all sub-suite tests */
+        subTestTotalTime: number;
+    };
+};
+
+export type { Test, Suite };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,46 +1,4 @@
 /**
- * A completed test's structure in suite metrics
- *
- * Include the test's name, start/end time & duration, overall test number, and test number within this suite
- */
-type Test = {
-    /** The test's name. Determined during startTest(), this is unique for the suite it's in */
-    readonly name: string;
-    /** Timestamp the test began, in microseconds. Obtained at the end of startTest() for maximal accuracy */
-    readonly startTimestamp: number;
-    /** Timestamp the test ended at, in microseconds. Obtained at the beginning of stopTest() for maximal accuracy */
-    readonly endTimestamp: number;
-    /** Duration the test took, in microseconds. Equal to endTimestamp - startTimestamp */
-    readonly duration: number;
-    /** Order this test was completed in for all tests in any suite. E.g. 34th test of 150 -> 34 */
-    readonly testNumber: number;
-    /** Order this test was completed in for this suite. E.g. the 3rd of 6 test in the suite -> 3 */
-    readonly suiteTestNumber: number;
-};
-
-/**
- * A test suite's structure in suite metrics
- *
- * Includes the suite's name, tests in the suite, sub-suites this suite has, and the number of tests in sub-suites
- */
-type Suite = {
-    /** The suite's name */
-    readonly name: string;
-    /** All tests directly in this suite (doesn't include sub-suites). Maps the test name to the Test type object */
-    readonly tests: Map<string, Test>;
-    /** All sub-suites directly within this suite (non-recursive). Maps the suite name to the Suite type object */
-    readonly subSuites: Map<string, Suite>;
-
-    /** Stores aggregate test data from sub-suites to prevent the need for recursive calls */
-    readonly subSuiteData: {
-        /** Number of tests in this suite AND in all sub-suites of this suite */
-        numSubTests: number;
-        /** Total test time for all sub-suite tests */
-        subTestTotalTime: number;
-    };
-};
-
-/**
  * Metric type for returned data: total # tests, total time, and average time for a given suite (or suites)
  *
  * Used to package test metrics together for suite/suites, such as getSuiteMetrics and getSuiteMetricsRecursive
@@ -91,4 +49,4 @@ type RecursiveSuiteData = {
     readonly totalTestMetrics: Metrics;
 }
 
-export type { Test, Suite, Metrics, SuiteData, RecursiveSuiteData };
+export type { Metrics, SuiteData, RecursiveSuiteData };
