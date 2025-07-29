50b13d3c3fcd77df9020425324bec627e7662f12
Reid Moffat <18rem8@queensu.ca>
2025-07-28 21:13:31 -0400

    Placed tests in unit folder

--- test/unit/BaseSuiteMetrics/getSuiteMetrics.test.ts
+++ test/unit/BaseSuiteMetrics/getSuiteMetrics.test.ts
@@ -1,8 +1,8 @@
 import { assert } from 'chai';
 import SuiteMetrics, { SuiteData } from "suite-metrics";
-import { createSimpleTestData, createNestedTestData } from "../generators/testDataHelpers.ts";
-import { _MockSuiteMetrics } from "../generators/mocks.js";
-import { sleep } from "../helpers.js";
+import { createSimpleTestData, createNestedTestData } from "../../generators/testDataHelpers.ts";
+import { _MockSuiteMetrics } from "../../generators/mocks.js";
+import { sleep } from "../../helpers.js";
 
 suite("[BaseSuiteMetrics] getSuiteMetrics", function() {
 
