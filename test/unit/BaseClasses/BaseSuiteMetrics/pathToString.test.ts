fe0bfcb0cc17aec0d6a91e957a3c364211bf63b2
Reid Moffat <18rem8@queensu.ca>
2025-08-19 17:51:31 -0400

    Setup path to string tests

---
+++ test/unit/BaseClasses/BaseSuiteMetrics/pathToString.test.ts
@@ -0,0 +1,8 @@
+import { BaseSuiteMetrics } from "suite-metrics";
+
+suite("[BaseSuiteMetrics] pathToString", function() {
+
+    test("", function() {
+        const result = BaseSuiteMetrics.pathToString([]);
+    });
+});
