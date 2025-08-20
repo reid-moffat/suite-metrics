49105f3a5ecf55bb4355b85eaef9d793bcc88311
Reid Moffat <18rem8@queensu.ca>
2025-08-20 14:57:23 -0400

    Test getSuite

--- test/unit/Query/getSuite.test.ts
+++ test/unit/Query/getSuite.test.ts
@@ -1,12 +1,16 @@
 import SuiteMetrics, { Suite } from "suite-metrics";
 import { createSimpleTestData } from "../../generators/testDataHelpers.js";
 import { assert } from "chai";
+import { DEFAULT_OPTIONS } from "../../generators/options.js";
 
 suite("[Query] getSuite", function () {
 
     test("Simple data", function () {
         const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
-
         const result: Suite = instance.queries.getSuite([]);
+        console.log(`Result: ${JSON.stringify(result, null, 4)}`);
+
+        assert.equal(result.tests.size, 0);
+        assert.equal(result.subSuites.size, DEFAULT_OPTIONS.numSuites);
     });
 });
