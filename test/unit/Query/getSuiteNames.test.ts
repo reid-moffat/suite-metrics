5a9a80c9c5c0ef0e708dc0be6ef7075285adf523
Reid Moffat <18rem8@queensu.ca>
2025-08-20 15:03:46 -0400

    Assertions for get suite names

--- test/unit/Query/getSuiteNames.test.ts
+++ test/unit/Query/getSuiteNames.test.ts
@@ -1,12 +1,19 @@
 import SuiteMetrics from "suite-metrics";
 import { createSimpleTestData } from "../../generators/testDataHelpers.js";
 import { assert } from "chai";
+import { DEFAULT_OPTIONS } from "../../generators/options.js";
 
 suite("[Query] getSuiteNames", function () {
 
     test("Simple data", function () {
         const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
+        const result: string[] = instance.queries.getSuiteNames([]);
+        console.log(`Result: ${JSON.stringify(result, null, 4)}`);
 
-        const result = instance.queries.getSuiteNames([]);
+        assert.equal(result.length, DEFAULT_OPTIONS.numSuites);
+        for (let i: number = 0; i < result.length; ++i) {
+            assert.isString(result[i]);
+            assert.equal(result[i], DEFAULT_OPTIONS.suiteNamePrefix + (i + 1));
+        }
     });
 });
