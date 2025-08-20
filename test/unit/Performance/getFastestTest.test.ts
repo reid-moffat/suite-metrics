069d690b6d3cc20c04f8eefaf187c4c31b1a6ec9
Reid Moffat <18rem8@queensu.ca>
2025-08-20 15:59:55 -0400

    Define fastest tests

---
+++ test/unit/Performance/getFastestTest.test.ts
@@ -0,0 +1,17 @@
+import SuiteMetrics, { Test } from "suite-metrics";
+import serialize from "serialize-javascript";
+import { createSimpleTestData } from "../../generators/testDataHelpers.js";
+import { assert } from "chai";
+
+suite("[Performance] getFastestTest", function () {
+
+    test("Simple data", function() {
+        const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
+        const result: Test = instance.performance.getFastestTest();
+        console.log(`Result: ${serialize(result, 4)}`);
+
+        assert.exists(result);
+        const allTests: Test[] = instance.performance.getAllTestsSlowestFirst();
+        assert.deepEqual(result, allTests[0]);
+    });
+});
