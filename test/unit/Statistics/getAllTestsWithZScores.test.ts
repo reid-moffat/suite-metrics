c25324b08f44294677189127080053dd8e095d0d
Reid Moffat <18rem8@queensu.ca>
2025-08-20 17:16:22 -0400

    Added assertions to all z scores

--- test/unit/Statistics/getAllTestsWithZScores.test.ts
+++ test/unit/Statistics/getAllTestsWithZScores.test.ts
@@ -6,12 +6,31 @@ import { DEFAULT_OPTIONS } from "../../generators/options.js";
 
 suite("[Statistics] getAllTestsWithZScores", function () {
 
+    /**
+     * Gets the minimum theoretical Z-score for a set of n tests
+     */
+    function getMinZScore(n: number) {
+        return -Math.sqrt(n - 1);
+    }
+
+    /**
+     * Gets the maximum theoretical Z-score for a set of n tests
+     */
+    function getMaxZScore(n: number) {
+        return Math.sqrt(n - 1);
+    }
+
     test("Simple data", function() {
         const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
-        const result: { test: Test, zScore: number }[] = instance.statistics.getAllTestsWithZScores();
-        console.log(`Result: ${serialize(result, 4)}`);
+        const tests: { test: Test, zScore: number }[] = instance.statistics.getAllTestsWithZScores();
+        console.log(`Result: ${serialize(tests, 4)}`);
 
-        assert.lengthOf(result, DEFAULT_OPTIONS.testsPerSuite * DEFAULT_OPTIONS.numSuites);
-        assert.lengthOf(result, instance.metrics.getTotalTestCount());
+        assert.lengthOf(tests, DEFAULT_OPTIONS.testsPerSuite * DEFAULT_OPTIONS.numSuites);
+        assert.lengthOf(tests, instance.metrics.getTotalTestCount());
+
+        for (let i: number = 0; i < tests.length; ++i) {
+            assert.isAtLeast(tests[i].zScore, getMinZScore(instance.metrics.getTotalTestCount()));
+            assert.isAtMost(tests[i].zScore, getMaxZScore(instance.metrics.getTotalTestCount()));
+        }
     });
 });
