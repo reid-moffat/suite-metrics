cbb004b869d8d3309ff203d2bcc50592d42b0635
Reid Moffat <18rem8@queensu.ca>
2025-09-11 17:56:04 -0400

    TODO 100k optimize

--- test/performance/performance.test.ts
+++ test/performance/performance.test.ts
@@ -53,15 +53,55 @@ suite("Performance", function () {
         });
     });
 
+    /* TODO
     suite("100k tests", function() {
-        test("100k tests", function() {
+        test("100k suites", function() {
+            const generatorOptions = {
+                numSuites: 9,
+                testsPerSuite: 1,
+                maxDepth: 5,
+                subSuitesPerSuite: 10,
 
+                minDuration: 500,
+                maxDuration: 50_000
+            };
+
+            const metrics = createNestedTestData(false, generatorOptions) as SuiteMetrics;
+
+            console.log(`Total tests: ` + metrics.metrics.getTotalTestCount());
+        });
+
+        test("Balanced", function() {
+            const generatorOptions = {
+                numSuites: 5,
+                testsPerSuite: 19,
+                maxDepth: 4,
+                subSuitesPerSuite: 10,
+
+                minDuration: 500,
+                maxDuration: 50_000
+            };
+
+            const metrics = createNestedTestData(false, generatorOptions) as SuiteMetrics;
+
+            console.log(`Total tests: ` + metrics.metrics.getTotalTestCount());
+        });
+
+        test("Mid depth", function() {
+            const generatorOptions = {
+                numSuites: 10,
+                testsPerSuite: 27,
+                maxDepth: 4,
+                subSuitesPerSuite: 7,
+
+                minDuration: 500,
+                maxDuration: 50_000
+            };
+
+            const metrics = createNestedTestData(false, generatorOptions) as SuiteMetrics;
+
+            console.log(`Total tests: ` + metrics.metrics.getTotalTestCount());
         });
     });
-
-    suite("1m tests", function() {
-        test("100k tests", function() {
-
-        });
-    });
+     */
 });
