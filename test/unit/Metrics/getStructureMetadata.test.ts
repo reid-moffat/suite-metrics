8d7c2f7b8e4384c416ee28f42d6e9f50d7a35dbd
Reid Moffat <18rem8@queensu.ca>
2025-09-15 17:01:37 -0400

    type spec

--- test/unit/Metrics/getStructureMetadata.test.ts
+++ test/unit/Metrics/getStructureMetadata.test.ts
@@ -1,4 +1,4 @@
-import SuiteMetrics from "suite-metrics";
+import SuiteMetrics, { StructureMetadata } from "suite-metrics";
 import { assert } from "chai";
 import { createNestedTestData } from "../../generators/testDataHelpers.ts";
 
@@ -7,6 +7,6 @@ suite("[Metrics] getStructureMetadata", function () {
     test("Simple data", function () {
         const instance: SuiteMetrics = createNestedTestData(false, { numSuites: 10, testsPerSuite: 10, maxDepth: 3 }) as SuiteMetrics;
 
-        const result = instance.metrics.getStructureMetadata();
+        const result: StructureMetadata = instance.metrics.getStructureMetadata();
     });
 });
