42470fc40641b97644ef20f377e4f4efae768cf6
Reid Moffat <18rem8@queensu.ca>
2025-09-16 16:29:07 -0400

    Suite validator

--- test/helpers/validators.ts
+++ test/helpers/validators.ts
@@ -1,5 +1,6 @@
+import { Suite, SuiteData } from "suite-metrics";
 import { expect } from 'chai';
-import { SuiteData } from "suite-metrics";
+import { assert } from "chai";
 
 // Required data to validate (total & average time differ each run)
 type SuiteDataValidate = {
@@ -74,4 +75,41 @@ const validateSuiteData = (data: SuiteData, expected: SuiteDataValidate) => {
     console.log(`✅ Total test metrics validated\n`);
 }
 
-export { validateSuiteData, SuiteDataValidate };
+/**
+ * Recursively validates a suite and all its sub-suites
+ */
+function validateSuiteRecursive(suite: Suite) {
+
+    // Validate top-level object
+    assert.isNotNull(suite, `Expected suite to not be null`);
+    assert.isObject(suite, `Expected suite to be an object`);
+    assert.isNotArray(suite, `Expected suite to not be an array`);
+    assert.isNotEmpty(suite, `Expected suite to not be empty`);
+
+    // Validate top-level keys exist
+    const expectedKeys: string[] = ["name", "path", "tests", "subSuites", "aggregateData"];
+    assert.hasAllKeys(suite, expectedKeys, `Expected suite ${JSON.stringify(suite)} to contain only keys ${JSON.stringify(expectedKeys)}`);
+
+    // Validate aggregate data object
+    const aggregateData = suite.aggregateData;
+    assert.isNotNull(aggregateData, `Expected aggregateData to not be null`);
+    assert.isObject(aggregateData, `Expected aggregateData to be an object`);
+    assert.isNotArray(aggregateData, `Expected aggregateData to not be an array`);
+    assert.isNotEmpty(aggregateData, `Expected aggregateData to not be empty`);
+
+    // Validate aggregate data keys exist
+    const expectedAggregateKeys: string[] = ["numTests", "totalTestTime"];
+    assert.hasAllKeys(aggregateData, expectedAggregateKeys, `Expected aggregateData ${JSON.stringify(aggregateData)} to contain only keys ${JSON.stringify(expectedAggregateKeys)}`);
+
+
+    // Validate all keys are the expected type
+    assert.isString(suite.name);
+    assert.isArray(suite.path);
+    assert.instanceOf(suite.tests, Map);
+    assert.instanceOf(suite.subSuites, Map);
+
+    assert.isNumber(suite.aggregateData.numTests);
+    assert.isNumber(suite.aggregateData.totalTestTime);
+}
+
+export { validateSuiteData, SuiteDataValidate, validateSuiteRecursive };
