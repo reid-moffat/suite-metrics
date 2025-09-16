7754dd21d0ef1120b479704d18ce1ad4b1a89047
Reid Moffat <18rem8@queensu.ca>
2025-09-16 15:43:49 -0400

    Split up get all data validation

--- test/unit/BaseClasses/BaseSuiteMetrics/getAllData.test.ts
+++ test/unit/BaseClasses/BaseSuiteMetrics/getAllData.test.ts
@@ -1,14 +1,20 @@
-import { Suite } from "suite-metrics";
+import SuiteMetrics, { BaseSuiteMetrics, Suite } from "suite-metrics";
 import { createSimpleTestData } from "../../../generators/testDataHelpers.js";
 import { assert } from "chai";
 
 suite("[BaseSuiteMetrics] getAllData", function() {
 
-    test("Simple test data", function() {
-        const instance = createSimpleTestData(false);
-
+    /**
+     * Gets and deeply validates a top-level suite
+     */
+    function runTest(instance: BaseSuiteMetrics) {
         const result: Suite = instance.getAllData();
 
         assert.equal(result.aggregateData.numTests, instance.metrics.getTotalTestCount());
+    }
+
+    test("Simple test data", function() {
+        const instance: SuiteMetrics = createSimpleTestData(false) as SuiteMetrics;
+        runTest(instance);
     });
 });
