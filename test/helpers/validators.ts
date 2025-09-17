7e2ffbb350e25fafcc47d2b1f874857cb06c1fde
Reid Moffat <18rem8@queensu.ca>
2025-09-17 11:30:22 -0400

    Update suite data validation to assert

--- test/helpers/validators.ts
+++ test/helpers/validators.ts
@@ -1,5 +1,4 @@
 import { Suite, Test, SuiteData } from "suite-metrics";
-import { expect } from 'chai';
 import { assert } from "chai";
 
 // Required data to validate (total & average time differ each run)
@@ -25,54 +24,58 @@ type SuiteDataValidate = {
  * @param expected Expected suite data (note: total and average time are left out since these vary each time)
  */
 const validateSuiteData = (data: SuiteData, expected: SuiteDataValidate) => {
+    assert.isObject(data);
+    assert.hasAllKeys(data, ['name', 'parentSuites', 'childSuites', 'directTestMetrics', 'subTestMetrics', 'totalTestMetrics']);
 
-    console.log(`Validating recursive suite metrics:\nResult: ${JSON.stringify(data, null, 4)}\nExpected: ${JSON.stringify(expected, null, 4)}`);
+    assert.isString(data.name);
+    assert.strictEqual(data.name, expected.name);
+    assert.deepStrictEqual(data.parentSuites, expected.parentSuites);
+    assert.deepStrictEqual(data.subSuites, expected.childSuites);
 
-    expect(data).to.be.an('object');
-    expect(data).to.have.all.keys(['name', 'parentSuites', 'childSuites', 'directTestMetrics', 'subTestMetrics', 'totalTestMetrics']);
-    console.log(`\n✅ Structure validated`);
-
-    expect(data.name).to.be.a('string');
-    expect(data.name).to.equal(expected.name);
-    expect(data.parentSuites).to.deep.equal(expected.parentSuites);
-    expect(data.subSuites).to.deep.equal(expected.childSuites);
-    console.log(`✅ Metadata validated`);
-
-    expect(data.directTestMetrics).to.have.all.keys(['numTests', 'totalTime', 'averageTime']);
-    expect(data.directTestMetrics.numTests).to.equal(expected.directTestMetrics.numTests);
+    assert.hasAllKeys(data.directTestMetrics, ['numTests', 'totalTime', 'averageTime']);
+    assert.strictEqual(data.directTestMetrics.numTests, expected.directTestMetrics.numTests);
     if (expected.directTestMetrics.numTests === 0) {
-        expect(data.directTestMetrics.totalTime).to.be.null;
-        expect(data.directTestMetrics.averageTime).to.be.null;
+        assert.isNull(data.directTestMetrics.totalTime);
+        assert.isNull(data.directTestMetrics.averageTime);
     } else {
-        expect(data.directTestMetrics.totalTime).to.be.a('number').and.be.above(0).and.satisfy((num: number) => Number.isInteger(num));
-        expect(data.directTestMetrics.averageTime).to.be.a('number').and.be.above(0).and.satisfy((num: number) => Number.isInteger(num)); // @ts-ignore
-        expect(data.directTestMetrics.averageTime).to.equal(data.directTestMetrics.totalTime / data.directTestMetrics.numTests);
+        assert.isNumber(data.directTestMetrics.totalTime);
+        assert.isAbove(data.directTestMetrics.totalTime, 0);
+        assert.isTrue(Number.isInteger(data.directTestMetrics.totalTime));
+        assert.isNumber(data.directTestMetrics.averageTime);
+        assert.isAbove(data.directTestMetrics.averageTime, 0);
+        assert.isTrue(Number.isInteger(data.directTestMetrics.averageTime)); // @ts-ignore
+        assert.strictEqual(data.directTestMetrics.averageTime, data.directTestMetrics.totalTime / data.directTestMetrics.numTests);
     }
-    console.log(`✅ Direct test metrics validated`);
 
-    expect(data.subTestMetrics).to.have.all.keys(['numTests', 'totalTime', 'averageTime']);
-    expect(data.subTestMetrics.numTests).to.equal(expected.subTestMetrics.numTests);
+    assert.hasAllKeys(data.subTestMetrics, ['numTests', 'totalTime', 'averageTime']);
+    assert.strictEqual(data.subTestMetrics.numTests, expected.subTestMetrics.numTests);
     if (expected.subTestMetrics.numTests === 0) {
-        expect(data.subTestMetrics.totalTime).to.be.null;
-        expect(data.subTestMetrics.averageTime).to.be.null;
+        assert.isNull(data.subTestMetrics.totalTime);
+        assert.isNull(data.subTestMetrics.averageTime);
     } else {
-        expect(data.subTestMetrics.totalTime).to.be.a('number').and.be.above(0).and.satisfy((num: number) => Number.isInteger(num));
-        expect(data.subTestMetrics.averageTime).to.be.a('number').and.be.above(0).and.satisfy((num: number) => Number.isInteger(num)); // @ts-ignore
-        expect(data.subTestMetrics.averageTime).to.equal(data.subTestMetrics.totalTime / data.subTestMetrics.numTests);
+        assert.isNumber(data.subTestMetrics.totalTime);
+        assert.isAbove(data.subTestMetrics.totalTime, 0);
+        assert.isTrue(Number.isInteger(data.subTestMetrics.totalTime));
+        assert.isNumber(data.subTestMetrics.averageTime);
+        assert.isAbove(data.subTestMetrics.averageTime, 0);
+        assert.isTrue(Number.isInteger(data.subTestMetrics.averageTime)); // @ts-ignore
+        assert.strictEqual(data.subTestMetrics.averageTime, data.subTestMetrics.totalTime / data.subTestMetrics.numTests);
     }
-    console.log(`✅ Sub test metrics validated`);
 
-    expect(data.totalTestMetrics).to.have.all.keys(['numTests', 'totalTime', 'averageTime']);
-    expect(data.totalTestMetrics.numTests).to.equal(expected.totalTestMetrics.numTests);
+    assert.hasAllKeys(data.totalTestMetrics, ['numTests', 'totalTime', 'averageTime']);
+    assert.strictEqual(data.totalTestMetrics.numTests, expected.totalTestMetrics.numTests);
     if (expected.totalTestMetrics.numTests === 0) {
-        expect(data.totalTestMetrics.totalTime).to.be.null;
-        expect(data.totalTestMetrics.averageTime).to.be.null;
+        assert.isNull(data.totalTestMetrics.totalTime);
+        assert.isNull(data.totalTestMetrics.averageTime);
     } else {
-        expect(data.totalTestMetrics.totalTime).to.be.a('number').and.be.above(0).and.satisfy((num: number) => Number.isInteger(num));
-        expect(data.totalTestMetrics.averageTime).to.be.a('number').and.be.above(0).and.satisfy((num: number) => Number.isInteger(num)); // @ts-ignore
-        expect(data.totalTestMetrics.averageTime).to.equal(data.totalTestMetrics.totalTime / data.totalTestMetrics.numTests);
+        assert.isNumber(data.totalTestMetrics.totalTime);
+        assert.isAbove(data.totalTestMetrics.totalTime, 0);
+        assert.isTrue(Number.isInteger(data.totalTestMetrics.totalTime));
+        assert.isNumber(data.totalTestMetrics.averageTime);
+        assert.isAbove(data.totalTestMetrics.averageTime, 0);
+        assert.isTrue(Number.isInteger(data.totalTestMetrics.averageTime)); // @ts-ignore
+        assert.strictEqual(data.totalTestMetrics.averageTime, data.totalTestMetrics.totalTime / data.totalTestMetrics.numTests);
     }
-    console.log(`✅ Total test metrics validated\n`);
 }
 
 /**
