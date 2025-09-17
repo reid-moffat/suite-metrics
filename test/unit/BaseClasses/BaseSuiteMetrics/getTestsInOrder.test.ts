55830c2aa28079d28d74395185a0f0694a8e2bc8
Reid Moffat <18rem8@queensu.ca>
2025-09-17 09:57:08 -0400

    Move to helper function

--- test/unit/BaseClasses/BaseSuiteMetrics/getTestsInOrder.test.ts
+++ test/unit/BaseClasses/BaseSuiteMetrics/getTestsInOrder.test.ts
@@ -1,14 +1,21 @@
-import { Test } from "suite-metrics";
+import SuiteMetrics, { BaseSuiteMetrics, Test } from "suite-metrics";
 import { createSimpleTestData } from "../../../generators/testDataHelpers.js";
 import { assert } from "chai";
+import { validateTest } from "../../../helpers/validators.js";
 
 suite("[BaseSuiteMetrics] getTestsInOrder", function() {
 
+    /**
+     * Gets and deeply validates all tests in order
+     */
+    function runTest(instance: BaseSuiteMetrics) {
+        const results: Test[] = instance.getTestsInOrder();
+
+        assert.equal(results.length, instance.metrics.getTotalTestCount());
+    }
+
     test("Simple test data", function() {
-        const instance = createSimpleTestData(false);
-
-        const result: Test[] = instance.getTestsInOrder();
-
-        assert.equal(result.length, instance.metrics.getTotalTestCount());
+        const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
+        runTest(instance);
     });
 });
