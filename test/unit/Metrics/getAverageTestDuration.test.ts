317122ab1f500f2dacd28f7f2b954580b235ae90
Reid Moffat <18rem8@queensu.ca>
2025-09-17 11:42:42 -0400

    TEst runner for average duration

--- test/unit/Metrics/getAverageTestDuration.test.ts
+++ test/unit/Metrics/getAverageTestDuration.test.ts
@@ -1,9 +1,23 @@
-import SuiteMetrics from "suite-metrics";
+import SuiteMetrics, { BaseSuiteMetrics, Test } from "suite-metrics";
 import { assert } from "chai";
 import { createSimpleTestData } from "../../generators/testDataHelpers.ts";
 import { DEFAULT_OPTIONS } from "../../generators/options.ts";
 
 suite("[Metrics] getAverageTestDuration", function () {
+
+    /**
+     * Gets and deeply validates the average test duration for an instance
+     */
+    function runTest(instance: BaseSuiteMetrics) {
+        const averageDuration: number = instance.metrics.getAverageTestDuration();
+        assert.isNumber(averageDuration, `Average duration must be a number`);
+
+        const allTests: Test[] = instance.getTestsInOrder();
+        const calculatedTotal: number = allTests.reduce((acc: number, curr: Test): number => acc + curr.duration, 0);
+        const calculatedAverage: number = calculatedTotal / allTests.length;
+
+        assert.strictEqual(averageDuration, calculatedAverage, `Expected and calculated average don't match`);
+    }
 
     test("Simple data", function () {
         const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
