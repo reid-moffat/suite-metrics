5f03bfe90c347dcb435c48a91042c195332a2a6f
Reid Moffat <18rem8@queensu.ca>
2025-09-17 10:03:54 -0400

    Added more tests

--- test/unit/BaseClasses/BaseSuiteMetrics/getTestsInOrder.test.ts
+++ test/unit/BaseClasses/BaseSuiteMetrics/getTestsInOrder.test.ts
@@ -1,5 +1,10 @@
-import SuiteMetrics, { BaseSuiteMetrics, Test } from "suite-metrics";
-import { createSimpleTestData } from "../../../generators/testDataHelpers.js";
+import SuiteMetrics, { BaseSuiteMetrics, ConcurrentSuiteMetrics, Test } from "suite-metrics";
+import {
+    createNestedTestData,
+    createPresetData,
+    createSimpleTestData,
+    PRESET_TYPE
+} from "../../../generators/testDataHelpers.js";
 import { assert } from "chai";
 import { validateTest } from "../../../helpers/validators.js";
 
@@ -18,8 +23,64 @@ suite("[BaseSuiteMetrics] getTestsInOrder", function() {
         }
     }
 
-    test("Simple test data", function() {
+    test("Simple data", function() {
         const instance: SuiteMetrics = createSimpleTestData() as SuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Simple data concurrent", function() {
+        const instance: ConcurrentSuiteMetrics = createSimpleTestData(true) as ConcurrentSuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Nested data", function() {
+        const instance: SuiteMetrics = createNestedTestData() as SuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Nested data concurrent", function() {
+        const instance: ConcurrentSuiteMetrics = createNestedTestData(true) as ConcurrentSuiteMetrics;
+        runTest(instance);
+    });
+
+
+    test("Normal preset", function() {
+        const instance: SuiteMetrics = createPresetData(false, PRESET_TYPE.NORMAL) as SuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Normal preset concurrent", function() {
+        const instance: ConcurrentSuiteMetrics = createPresetData(true, PRESET_TYPE.NORMAL) as ConcurrentSuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Large preset", function() {
+        const instance: SuiteMetrics = createPresetData(false, PRESET_TYPE.LARGE_SUITE) as SuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Large preset concurrent", function() {
+        const instance: ConcurrentSuiteMetrics = createPresetData(true, PRESET_TYPE.LARGE_SUITE) as ConcurrentSuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Realistic preset", function() {
+        const instance: SuiteMetrics = createPresetData(false, PRESET_TYPE.REALISTIC_PREMADE) as SuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Realistic preset concurrent", function() {
+        const instance: ConcurrentSuiteMetrics = createPresetData(true, PRESET_TYPE.REALISTIC_PREMADE) as ConcurrentSuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Edge case preset", function() {
+        const instance: SuiteMetrics = createPresetData(false, PRESET_TYPE.EDGE_CASES) as SuiteMetrics;
+        runTest(instance);
+    });
+
+    test("Edge case preset concurrent", function() {
+        const instance: ConcurrentSuiteMetrics = createPresetData(true, PRESET_TYPE.EDGE_CASES) as ConcurrentSuiteMetrics;
         runTest(instance);
     });
 });
