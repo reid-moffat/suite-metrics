e0dc931b1077bc3fbf07175aca485598a0004ceb
Reid Moffat <18rem8@queensu.ca>
2025-09-17 10:04:36 -0400

    TS imports

--- test/unit/BaseClasses/BaseSuiteMetrics/getTestsInOrder.test.ts
+++ test/unit/BaseClasses/BaseSuiteMetrics/getTestsInOrder.test.ts
@@ -1,12 +1,12 @@
 import SuiteMetrics, { BaseSuiteMetrics, ConcurrentSuiteMetrics, Test } from "suite-metrics";
+import { assert } from "chai";
 import {
     createNestedTestData,
     createPresetData,
     createSimpleTestData,
     PRESET_TYPE
-} from "../../../generators/testDataHelpers.js";
-import { assert } from "chai";
-import { validateTest } from "../../../helpers/validators.js";
+} from "../../../generators/testDataHelpers.ts";
+import { validateTest } from "../../../helpers/validators.ts";
 
 suite("[BaseSuiteMetrics] getTestsInOrder", function() {
 
